5eaf90f7e53d8e61832c99d761cf4fc4e8d281bd
Bharat-00 <bharat.trivedi_cs22@gla.ac.in>
2026-03-11 15:59:30 +0530

    [Bharat]Add.added the uc4

---
+++ uc4/src/main/java/com/bharat/QuantityMeasurementApp.java
@@ -0,0 +1,24 @@
+package com.bharat;
+public class QuantityMeasurementApp {
+    public static void main(String[] args) {
+        System.out.println("UC4 ");
+        // 1 Yard = 3 Feet
+        System.out.println(new Length(1.0, Length.LengthUnit.YARDS)
+                .equals(new Length(3.0, Length.LengthUnit.FEET)));  // true
+        // 1 Yard = 36 Inches
+        System.out.println(new Length(1.0, Length.LengthUnit.YARDS)
+                .equals(new Length(36.0, Length.LengthUnit.INCHES))); // true
+        // Yard to Yard
+        System.out.println(new Length(2.0, Length.LengthUnit.YARDS)
+                .equals(new Length(2.0, Length.LengthUnit.YARDS))); // true
+        // Centimeter to Centimeter
+        System.out.println(new Length(2.0, Length.LengthUnit.CENTIMETERS)
+                .equals(new Length(2.0, Length.LengthUnit.CENTIMETERS))); // true
+        // 1 cm = 0.393701 inches
+        System.out.println(new Length(1.0, Length.LengthUnit.CENTIMETERS)
+                .equals(new Length(0.393701, Length.LengthUnit.INCHES))); // true
+        // Negative case
+        System.out.println(new Length(1.0, Length.LengthUnit.YARDS)
+                .equals(new Length(2.0, Length.LengthUnit.FEET))); // false
+    }
+}
---
+++ uc4/src/test/java/com/bharat/QuantityMeasurementAppTest.java
@@ -0,0 +1,53 @@
+package com.bharat;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import org.junit.jupiter.api.Test;
+public class QuantityMeasurementAppTest {
+   // Yard Tests
+    @Test
+    public void testYardToYardEquality() {
+        assertTrue(new Length(1.0, Length.LengthUnit.YARDS)
+                .equals(new Length(1.0, Length.LengthUnit.YARDS)));
+    }
+    @Test
+    public void testYardToFeetEquality() {
+        assertTrue(new Length(1.0, Length.LengthUnit.YARDS)
+                .equals(new Length(3.0, Length.LengthUnit.FEET)));
+    }
+    @Test
+    public void testYardToInchEquality() {
+        assertTrue(new Length(1.0, Length.LengthUnit.YARDS)
+                .equals(new Length(36.0, Length.LengthUnit.INCHES)));
+    }
+    @Test
+    public void testYardToFeetInequality() {
+        assertFalse(new Length(1.0, Length.LengthUnit.YARDS)
+                .equals(new Length(2.0, Length.LengthUnit.FEET)));
+    }
+    // CM Tests
+    @Test
+    public void testCmToCmEquality() {
+        assertTrue(new Length(2.0, Length.LengthUnit.CENTIMETERS)
+                .equals(new Length(2.0, Length.LengthUnit.CENTIMETERS)));
+    }
+    @Test
+    public void testCmToInchEquality() {
+        assertTrue(new Length(1.0, Length.LengthUnit.CENTIMETERS)
+                .equals(new Length(0.393701, Length.LengthUnit.INCHES)));
+    }
+    @Test
+    public void testCmToFeetInequality() {
+        assertFalse(new Length(1.0, Length.LengthUnit.CENTIMETERS)
+                .equals(new Length(1.0, Length.LengthUnit.FEET)));
+    }
+    // Transitive property UC4
+    @Test
+    public void testMultiUnitTransitiveProperty() {
+        Length A = new Length(1.0, Length.LengthUnit.YARDS);
+        Length B = new Length(3.0, Length.LengthUnit.FEET);
+        Length C = new Length(36.0, Length.LengthUnit.INCHES);
+        assertTrue(A.equals(B));
+        assertTrue(B.equals(C));
+        assertTrue(A.equals(C));
+    }
+}
