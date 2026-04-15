36e8a056c0947fd13821341794d9740528299b3e
Bharat-00 <bharat.trivedi_cs22@gla.ac.in>
2026-04-15 23:07:55 +0530

    [Bharat]Add.added the uc17

---
+++ uc18/src/main/java/com/bharat/controller/AuthController.java
@@ -0,0 +1,37 @@
+package com.bharat.controller;
+
+import com.bharat.entity.User;
+import com.bharat.service.AuthService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
+
+@CrossOrigin(origins = "http://localhost:4200")
+@RestController
+@RequestMapping("/auth")
+public class AuthController {
+
+    @Autowired
+    private AuthService authService;
+
+    // LOGIN
+    @PostMapping("/login")
+    public ResponseEntity<?> login(@RequestBody User user) {
+        String token = authService.login(user);
+
+        return ResponseEntity.ok(
+                Map.of("token", token, "username", user.getUsername())
+        );
+    }
+
+    // SIGNUP
+    @PostMapping("/signup")
+    public ResponseEntity<?> signup(@RequestBody User user) {
+        String message = authService.signup(user);
+        return ResponseEntity.ok(
+                Map.of("message", message)
+        );
+    }
+}
